20832a6aefc7d636fea01facdfc34dc1361e37dd
thezn0x <thezn0x.exe@gmail.com>
2026-01-13 18:12:39 +0500

    REF: Made extractors and transformers 10x more scalable

--- scripts/run_extractors.py
+++ scripts/run_extractors.py
@@ -5,47 +5,50 @@ from config.settings import EXTRACTORS
 
 logger = get_logger(__name__)
 
-rozee_cfg = EXTRACTORS["rozee"]
-careerjet_cfg = EXTRACTORS["careerjet"]
+EXTRACTORS_MAP = {
+    "rozee": RozeeExtractor,
+    "careerjet":CareerjetExtractor
+}
 
 def main():
-    rozee_jobs = []
-    careerjet_jobs = []
-
-    rozee_extractor = None
-    careerjet_extractor = None
-
     try:
-        if rozee_cfg["enabled"]:
-            logger.info("Starting Rozee.pk scraper...")
-            rozee_extractor = RozeeExtractor(
-                base_url=rozee_cfg["base_url"],
-                card=rozee_cfg["card"]
-            )
-            rozee_jobs = rozee_extractor.fetch_jobs(rozee_cfg["max_pages"])
+        overall_success = True
+        for name, config in EXTRACTORS.items():
+            if isinstance(config, dict):
+                continue
+            
+            if not config.get("enabled", False):
+                continue
 
-        if careerjet_cfg["enabled"]:
-            logger.info("Starting CareerJet.pk scraper...")
-            careerjet_extractor = CareerjetExtractor(
-                base_url= careerjet_cfg["base_url"],
-                card=careerjet_cfg["card"]
-            )
-            careerjet_jobs = careerjet_extractor.fetch_jobs(careerjet_cfg["max_pages"])
+            try:
+                extractor = EXTRACTORS_MAP.get(name)(
+                    base_url=config.get("base_url"),
+                    card=config.get("card")
+                )
+                logger.info(f"Starting {name} extractor...")
+                if not extractor:
+                    logger.warning(f"Couldn't find {name}, skipping to next extractor.")
+                    continue
+
+                jobs  = extractor.fetch_jobs(config.get("max_pages"))
+                if not jobs:
+                    logger.warning(f"No jobs found in {name}.")
+                    continue
+
+                try:
+                    extractor.save_jobs(config.get("output_path"),jobs)
+                    logger.info(f"Saved {len(jobs)} jobs from {name} to {config.get("output_path")}...")
+                except Exception:
+                    logger.exception(f"Error while saving jobs from {name}.")
+
+            except Exception:
+                logger.error(f"Error while extracting jobs from {name}.")
 
     except Exception:
-        logger.exception("Fatal error in run_extractors")
-        return False
+        logger.exception(f"Critical error while performing {__name__}")
+        overall_success = False
 
-    if rozee_jobs and rozee_extractor:
-        rozee_extractor.save_jobs(rozee_cfg["output_path"], rozee_jobs)
-        logger.info(f"Saved rozee_jobs to {rozee_cfg['output_path']}")
-
-    if careerjet_jobs and careerjet_extractor:
-        careerjet_extractor.save_jobs(careerjet_cfg['output_path'], careerjet_jobs)
-        logger.info(f"Saved careerjet_jobs to {careerjet_cfg['output_path']}")
-
-    return True
-
+    return overall_success
 
 if __name__ == "__main__":
     main()
--- scripts/run_transformers.py
+++ scripts/run_transformers.py
@@ -4,47 +4,44 @@ from src.utils.logger import get_logger
 from pathlib import Path
 from config.settings import TRANSFORMERS,EXTRACTORS
 
-rozee_cfg = TRANSFORMERS["rozee"]
-careerjet_cfg = TRANSFORMERS["careerjet"]
-rozee_cfg_ex = EXTRACTORS["rozee"]["output_path"]
-careerjet_cfg_ex = EXTRACTORS["careerjet"]["output_path"]
-
-
 # Logger
 logger = get_logger(__name__)
 
+
+TRANSFORMERS_MAP = {
+    "rozee": Cleaner,
+    "careerjet": Cleaner
+}
+
 def main():
     try:
-        careerjet_path = Path(careerjet_cfg_ex)
-        rozee_path = Path(rozee_cfg_ex)
-        cleaned_careerjet_jobs = []
-        cleaned_rozee_jobs = []
+        overall_success = True
+        for name, config in TRANSFORMERS.items():
+            if not isinstance(config, dict):
+                continue
 
-        if careerjet_path.exists():
-            careerjet_cleaner = Cleaner(extractor_name='careerjet')
-            with open(careerjet_cfg_ex) as f:
-                careerjet_job_data = json.load(f)
-            cleaned_careerjet_jobs = careerjet_cleaner.clean_jobs(careerjet_job_data)
-        else:
-            logger.error(f"Careerjet jobs not found in {careerjet_cfg_ex}")
-        if rozee_path.exists():
-            rozee_cleaner = Cleaner(extractor_name='rozee')
-            with open(rozee_cfg_ex) as f:
-                rozee_job_data = json.load(f)
-            cleaned_rozee_jobs = rozee_cleaner.clean_jobs(rozee_job_data)
-        else:
-            logger.error(f"Rozee jobs not found in {rozee_cfg_ex}")
+            if not config.get("enabled", False):
+                continue
 
-        if cleaned_careerjet_jobs:
-            careerjet_cleaner.save_jobs(careerjet_cfg["output_path"],cleaned_careerjet_jobs)
+            input_path = EXTRACTORS[name]["output_path"]
+            if not Path(input_path).exists():
+                logger.warning(f"No data from {name} extractor")
+                continue
 
-        if cleaned_rozee_jobs:
-            rozee_cleaner.save_jobs(rozee_cfg["output_path"],cleaned_rozee_jobs)
-        return True
+            with open(input_path) as f:
+                jobs = json.load(f)
 
+            cleaner = Cleaner(extractor_name=name)
+            cleaned = cleaner.clean_jobs(jobs)
+
+            cleaner.save_jobs(config["output_path"], cleaned)
+            logger.info(f"Cleaned {len(cleaned)} jobs from {name}")
+        
     except Exception:
-        logger.exception("Error while performing %s",__name__)
-        return False
+        logger.exception(f"Critical error while performing {__name__}")
+        overall_success = False
+
+    return overall_success
 
 if __name__ == "__main__":
     main()
