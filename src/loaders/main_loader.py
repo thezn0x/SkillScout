8617b15936b67a838bea93f574bcf7f5d1712da5
thezn0x <thezn0x.exe@gmail.com>
2026-01-20 17:45:14 +0500

    FEAT: Made base loader's load_jobs() function

--- src/loaders/main_loader.py
+++ src/loaders/main_loader.py
@@ -1,8 +1,9 @@
-from typing import List,Dict,Any
-import json
+from config.settings import TRANSFORMERS
 from config.settings import LOADERS
-from znpg import Database
+from typing import List,Dict,Any
 from dotenv import load_dotenv
+from znpg import Database
+import json
 import os
 
 load_dotenv(LOADERS["dotenv_path"])
@@ -19,8 +20,27 @@ class Loader:
                 data.extend(_data)
         return data
 
-    def load(self,table , data: List[Dict[str,Any]]):
-        conn_string = os.getenv("DATABASE_URL")
+    def load_companies(self,table , data: List[Dict[str,Any]]):
+        companies = []
+        for job in data:
+            company= job.get("company",None)
+            companies.append(company)
+
+        unique_companies = list(set(companies))
+        loading_list = []
+        for comp in unique_companies:
+            loading_list.append({"name":comp})
+
         with Database() as db:
-            db.url_connect(conn_string=conn_string)
-            db.bulk_insert(table=table,data=data)
+            db.url_connect(os.getenv("DATABASE_URL"))
+            db.bulk_insert(table,loading_list)
+            keys = db.select(table,["company_id","name"])
+        return keys
+
+
+# test:
+if __name__ == "__main__":
+    loader = Loader("loader")
+    with open(TRANSFORMERS["rozee"]["output_path"],"r") as f:
+        data = json.load(f)
+    loader.load_companies("companies",data)
