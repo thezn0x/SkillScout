a2e5e8ac9ad17b8a653983d8afd821aea2d3f3c9
thezn0x <thezn0x.exe@gmail.com>
2026-01-22 15:45:15 +0500

    FEAT: Abstracted repeated patterns and made load_locations

--- scripts/run_loaders.py
+++ scripts/run_loaders.py
@@ -6,10 +6,11 @@ from src.loaders.main_loader import Loader
 from config.settings import LOADERS
 
 loader = Loader("loader")
-with open(TRANSFORMERS["rozee"]["output_path"],"r") as f:
+with open(TRANSFORMERS["careerjet"]["output_path"],"r") as f:
     data = json.load(f)
-    loader.load_companies("companies",data)
-    loader.load_skills("skills",data)
-        
-loader = Loader(__name__)
-loader.load_platforms("platforms",LOADERS)
+#     loader.load_companies("companies",data)
+#     loader.load_skills("skills",data)
+#
+# loader = Loader(__name__)
+# loader.load_platforms("platforms",LOADERS)
+loader.load_locations("blah",data,)
--- src/loaders/main_loader.py
+++ src/loaders/main_loader.py
@@ -23,9 +23,16 @@ class Loader:
         else:
             unique_data = list(set(non_unique))
         return unique_data
+    @staticmethod
+    def load_get_keys(table:str, to_load:List[Dict[str,Any]], columns:List[str]) -> List[Dict[str,Any]]:
+        with Database() as db:
+            db.url_connect(os.getenv("DATABASE_URL"))
+            db.bulk_insert(table,to_load)
+            keys = db.select(table, columns)
+        return keys
 
-
-    def get_data(self, data_map:Dict[str,Any]) -> List[Dict[str,Any]]:
+    @staticmethod
+    def get_data(data_map:Dict[str,Any]) -> List[Dict[str,Any]]:
         data = []
         for name,path in data_map.items():
             with open(path,"r") as f:
@@ -37,24 +44,17 @@ class Loader:
         companies = self.get_unique(data,"company")
         loading_list = []
         for company in companies:
-            loading_list.append({"name":company})
-        with Database() as db:
-            db.url_connect(os.getenv("DATABASE_URL"))
-            db.bulk_insert(table,loading_list)
-            keys = db.select(table,["company_id","name"])
-        return keys
-    
-    def load_skills(self,table:str ,data:str) -> List[Dict[str,Any]]:
+            if company != "N/A":
+                loading_list.append({"name":company})
+        return self.load_get_keys(table,loading_list,["company_id","name"])
+
+    def load_skills(self,table:str ,data:List[Dict[str,Any]]) -> List[Dict[str,Any]]:
         placeholder_skills = self.get_unique(data,"core_skills")
         skills = list(set(placeholder_skills))
         to_load = []
         for skill in skills:
             to_load.append({"skill_name":skill})
-        with Database() as db:
-            db.url_connect(os.getenv("DATABASE_URL"))
-            db.bulk_insert(table,to_load)
-            keys = db.select(table,["skill_id","skill_name"])
-        return keys
+        return self.load_get_keys(table,to_load,["skill_id","skill_name"])
 
     def load_platforms(self, table: str, platforms_config: Dict[str, Any]) -> List[Dict[str, Any]]:
         all_platforms = []
@@ -63,8 +63,12 @@ class Loader:
             if isinstance(platform_data, dict):
                 platform_data["platform_name"] = platform_name
                 all_platforms.append(platform_data)
-        with Database() as db:
-            db.url_connect(os.getenv("DATABASE_URL"))
-            db.bulk_insert(table, all_platforms)
-            keys = db.select(table, ["platform_id", "platform_name"])
-        return keys
+        return self.load_get_keys(table,all_platforms,["platform_id", "platform_name"])
+
+    def load_locations(self, table:str, data:List[Dict[str,Any]]):
+        locations = self.get_unique(data, "location")
+        location_list = []
+        for location in locations:
+            string_parts = location.split(",")
+            location_list.append({"city": string_parts[0].strip(), "country": string_parts[1].strip()})
+        return self.load_get_keys(table,location_list,["location_id","city"])
