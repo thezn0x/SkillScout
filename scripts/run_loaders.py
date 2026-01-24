436ddeaa513dc5becef5105c7e396ad1691538cd
thezn0x <thezn0x.exe@gmail.com>
2026-01-24 14:46:49 +0500

    Full ETL completed, moving to analytics. Proud of myself

--- scripts/run_loaders.py
+++ scripts/run_loaders.py
@@ -1,16 +1,43 @@
-# BETA TEST
-
 from config.settings import TRANSFORMERS
-import json
 from src.loaders.main_loader import Loader
 from config.settings import LOADERS
+from typing import Dict,Any,Union
+from src.utils.logger import get_logger
 
-loader = Loader("loader")
-with open(TRANSFORMERS["careerjet"]["output_path"],"r") as f:
-    data = json.load(f)
-#     loader.load_companies("companies",data)
-#     loader.load_skills("skills",data)
-#
-# loader = Loader(__name__)
-# loader.load_platforms("platforms",LOADERS)
-loader.load_locations("blah",data,)
+logger = get_logger(__name__)
+DATA_MAP = {
+    "rozee" : TRANSFORMERS["rozee"]["output_path"],
+    "careerjet" : TRANSFORMERS["careerjet"]["output_path"]
+}
+
+def main() -> Union[Dict[str,Any],None]:
+    logger.info("Starting Loader...")
+    try:
+        loader = Loader(__name__)
+        data = loader.get_data(DATA_MAP)
+        skill_keys = loader.load_skills("skills",data)
+        location_keys = loader.load_locations("locations",data)
+        company_keys = loader.load_companies("companies",data)
+        platform_keys = loader.load_platforms("platforms",LOADERS)
+        job_keys = loader.load_jobs("jobs",data,company_keys)
+        job_skills_keys = loader.load_job_skills("job_skills",data,job_keys,skill_keys)
+        job_location_keys = loader.load_job_locations("job_locations",data,job_keys, location_keys)
+        job_platform_keys = loader.load_job_platforms("job_platforms",data,job_keys, platform_keys)
+        keys = {
+            'skill_keys' : skill_keys,
+            'location_keys' : location_keys,
+            'company_keys' : company_keys,
+            'platform_keys' : platform_keys,
+            'job_keys' : job_keys,
+            'job_skills_keys' : job_skills_keys,
+            'job_location_keys' : job_location_keys,
+            'job_platform_keys' : job_platform_keys
+        }
+        logger.info("Loaded all data successfully")
+        return keys
+    except Exception as e:
+        logger.error("Error while performing %s: %s",__name__,e)
+        return None
+
+if __name__ == "__main__":
+    main()
--- src/loaders/main_loader.py
+++ src/loaders/main_loader.py
@@ -1,129 +1,179 @@
+from src.utils.logger import get_logger
 from config.settings import LOADERS
-from typing import List,Dict,Any
+from typing import List, Dict, Any, Union
 from dotenv import load_dotenv
 from znpg import Database
 import json
 import os
 
 load_dotenv(LOADERS["dotenv_path"])
-
+logger = get_logger(__name__)
 class Loader:
     def __init__(self,name:str):
         self.name = name
 
     @staticmethod
-    def get_unique(data:List[Dict[str,Any]],field:str) -> List[Any]:
-        non_unique = []
-        for one in data:
-            non_unique.append(one.get(field,None))
-        if isinstance(non_unique[0],list):
-            unique_data = []
-            for one in non_unique:
-                unique_data.extend(one)
-        else:
-            unique_data = list(set(non_unique))
-        return unique_data
-    @staticmethod
-    def load_get_keys(table:str, to_load:List[Dict[str,Any]], columns:List[str]) -> List[Dict[str,Any]]:
-        with Database() as db:
-            db.url_connect(os.getenv("DATABASE_URL"))
-            db.bulk_insert(table,to_load,"DO NOTHING")
-            keys = db.select(table, columns)
-        return keys
-
-    @staticmethod
-    def get_data(data_map:Dict[str,Any]) -> List[Dict[str,Any]]:
-        data = []
-        for name,path in data_map.items():
-            with open(path,"r") as f:
-                _data = json.load(f)
-                data.extend(_data)
-        return data
-
-    def load_companies(self,table:str , data: List[Dict[str,Any]]) -> List[Dict[str,Any]]:
-        companies = self.get_unique(data,"company")
-        loading_list = []
-        for company in companies:
-            if company != "N/A":
-                loading_list.append({"name":company})
-        return self.load_get_keys(table,loading_list,["company_id","name"])
-
-    def load_skills(self,table:str ,data:List[Dict[str,Any]]) -> List[Dict[str,Any]]:
-        placeholder_skills = self.get_unique(data,"core_skills")
-        skills = list(set(placeholder_skills))
-        to_load = []
-        for skill in skills:
-            to_load.append({"skill_name":skill})
-        return self.load_get_keys(table,to_load,["skill_id","skill_name"])
-
-    def load_platforms(self, table: str, platforms_config: Dict[str, Any]) -> List[Dict[str, Any]]:
-        all_platforms = []
-        platforms = platforms_config.get("platforms", platforms_config)
-        for platform_name, platform_data in platforms.items():
-            if isinstance(platform_data, dict):
-                platform_data["platform_name"] = platform_name
-                all_platforms.append(platform_data)
-        return self.load_get_keys(table,all_platforms,["platform_id", "platform_name"])
-
-    def load_locations(self, table:str, data:List[Dict[str,Any]]) -> List[Dict[str, Any]]:
-        locations = self.get_unique(data, "location")
-        location_list = []
-        for location in locations:
-            string_parts = location.split(",")
-            if len(string_parts) > 1:
-                location_list.append({"city": string_parts[0].strip(), "country": string_parts[1].strip()})
+    def get_unique(data:List[Dict[str,Any]],field:str) -> Union[None,List[Any]]:
+        try:
+            non_unique = []
+            for one in data:
+                non_unique.append(one.get(field,None))
+            if isinstance(non_unique[0],list):
+                unique_data = []
+                for one in non_unique:
+                    unique_data.extend(one)
             else:
-                location_list.append({"city":string_parts[0].strip(),"country":"Pakistan"})
-        return self.load_get_keys(table,location_list,["location_id","city","country"])
+                unique_data = list(set(non_unique))
+            return unique_data
+        except Exception as e:
+            logger.exception("Critical error while performing %s: %s",__name__,e)
 
-    def load_jobs(self, table:str, data:List[Dict[str,Any]], company_keys:List[Dict[str,Any]]) -> List[Dict[str, Any]]:
-        jobs_list = []
-        company_map = {c["name"]: c["company_id"] for c in company_keys}
-        for job in data:
-            if job["company"] not in company_map:
-                continue
-            job_data = {
-                "title": job["title"],
-                "company_id": company_map[job["company"]],
-                "description": job.get("description"),
-                "application_url": job.get("application_url"),
-                "min_salary": job.get("min_salary"),
-                "max_salary": job.get("max_salary"),
-                "posted_date": job.get("posted_date"),
-                "min_experience": job.get("min_experience"),
-                "salary_currency": "PKR",
-                "scraped_date": job.get("scraped_date")
-            }
-            jobs_list.append(job_data)
-        return self.load_get_keys(table,jobs_list,['job_id',"title","application_url"])
+    @staticmethod
+    def load_get_keys(table:str, to_load:List[Dict[str,Any]], columns:List[str]) -> Union[None,List[Dict[str,Any]]]:
+        try:
+            with Database() as db:
+                db.url_connect(os.getenv("DATABASE_URL"))
+                db.bulk_insert(table,to_load,"DO NOTHING")
+                keys = db.select(table, columns)
+            return keys
+        except Exception as e:
+            logger.exception("Critical error while performing %s: %s", __name__,e)
 
-    def load_job_skills(self, table:str, data:List[Dict[str,Any]], job_keys:List[Dict[str,Any]], skill_keys:List[Dict[str,Any]]) -> List[Dict[str,Any]]:
-        job_lookup = {job["application_url"]:job["job_id"] for job in job_keys}
-        skill_lookup = {skill["skill_name"]:skill["skill_id"] for skill in skill_keys}
-        junc_list = []
-        for job in data:
-            if job.get("application_url") not in job_lookup:
-                continue
-            job_id = job_lookup.get("application_url")
-            for skill in job.get("skills",[]):
-                if skill not in skill_lookup:
+    @staticmethod
+    def get_data(data_map:Dict[str,Any]) -> Union[None,List[Dict[str,Any]]]:
+        try:
+            data = []
+            for name,path in data_map.items():
+                with open(path,"r") as f:
+                    _data = json.load(f)
+                    data.extend(_data)
+            return data
+        except Exception as e:
+            logger.exception("Critical error while performing %s: %s", __name__,e)
+
+    def load_companies(self,table:str , data: List[Dict[str,Any]]) -> Union[List[Dict[str,Any]],None]:
+        try:
+            companies = self.get_unique(data,"company")
+            loading_list = []
+            for company in companies:
+                if company != "N/A":
+                    loading_list.append({"name":company})
+            return self.load_get_keys(table,loading_list,["company_id","name"])
+        except Exception as e:
+            logger.exception("Critical error while performing %s: %s", __name__,e)
+
+    def load_skills(self,table:str ,data:List[Dict[str,Any]]) -> Union[List[Dict[str,Any]],None]:
+        try:
+            placeholder_skills = self.get_unique(data,"core_skills")
+            skills = list(set(placeholder_skills))
+            to_load = []
+            for skill in skills:
+                to_load.append({"skill_name":skill})
+            return self.load_get_keys(table,to_load,["skill_id","skill_name"])
+        except Exception as e:
+            logger.exception("Critical error while performing %s: %s", __name__,e)
+
+    def load_platforms(self, table: str, platforms_config: Dict[str, Any]) -> Union[None,List[Dict[str, Any]]]:
+        try:
+            all_platforms = []
+            platforms = platforms_config.get("platforms", platforms_config)
+            for platform_name, platform_data in platforms.items():
+                if isinstance(platform_data, dict):
+                    platform_data["platform_name"] = platform_name
+                    all_platforms.append(platform_data)
+            return self.load_get_keys(table,all_platforms,["platform_id", "platform_name"])
+        except Exception as e:
+            logger.exception("Critical error while performing %s: %s", __name__,e)
+
+    def load_locations(self, table:str, data:List[Dict[str,Any]]) -> Union[None,List[Dict[str, Any]]]:
+        try:
+            locations = self.get_unique(data, "location")
+            location_list = []
+            for location in locations:
+                string_parts = location.split(",")
+                if len(string_parts) > 1:
+                    location_list.append({"city": string_parts[0].strip(), "country": string_parts[1].strip()})
+                else:
+                    location_list.append({"city":string_parts[0].strip(),"country":"Pakistan"})
+            return self.load_get_keys(table,location_list,["location_id","city","country"])
+        except Exception as e:
+            logger.exception("Critical error while performing %s: %s", __name__,e)
+
+    def load_jobs(self, table:str, data:List[Dict[str,Any]], company_keys:List[Dict[str,Any]]) -> Union[None,List[Dict[str, Any]]]:
+        try:
+            jobs_list = []
+            company_map = {c["name"]: c["company_id"] for c in company_keys}
+            for job in data:
+                if job["company"] not in company_map:
                     continue
-                skill_id = skill_lookup.get("skill_name")
-                junc_list.append({job_id:skill_id})
-        return self.load_get_keys(table,junc_list,["job_id","skill_id"])
+                job_data = {
+                    "title": job["title"],
+                    "company_id": company_map[job["company"]],
+                    "description": job.get("description"),
+                    "application_url": job.get("application_url"),
+                    "min_salary": job.get("min_salary"),
+                    "max_salary": job.get("max_salary"),
+                    "posted_date": job.get("posted_date"),
+                    "min_experience": job.get("min_experience"),
+                    "salary_currency": "PKR",
+                    "scraped_date": job.get("scraped_date")
+                }
+                jobs_list.append(job_data)
+            return self.load_get_keys(table,jobs_list,['job_id',"title","application_url"])
+        except Exception as e:
+            logger.exception("Critical error while performing %s: %s", __name__,e)
 
-    def load_job_locations(self, table:str, data:List[Dict[str,Any]],job_keys:List[Dict[str,Any]], location_keys:List[Dict[str,Any]]) -> List[Dict[str,Any]]:
-        job_lookup = {job["application_url"]:job["job_id"] for job in job_keys}
-        location_lookup = {(location["city"],location["country"]):location["location_id"] for location in location_keys}
-        jun_list = []
-        for job in data:
-            job_id = job_lookup[job["application_url"]]
-            location_str = job.get('location')
-            parts = location_str.split(",")
-            city = parts[0]
-            country = parts[1] if len(parts)>1 else "Pakistan"
-            loc = (city, country)
-            if loc in location_lookup:
-                loc_id = location_lookup[loc]
-                jun_list.append({job_id:loc_id})
-        return self.load_get_keys(table,jun_list,["job_id","location_id"])
+    def load_job_skills(self, table:str, data:List[Dict[str,Any]], job_keys:List[Dict[str,Any]], skill_keys:List[Dict[str,Any]]) -> Union[None,List[Dict[str,Any]]]:
+        try:
+            job_lookup = {job["application_url"]:job["job_id"] for job in job_keys}
+            skill_lookup = {skill["skill_name"]:skill["skill_id"] for skill in skill_keys}
+            junc_list = []
+            for job in data:
+                if job.get("application_url") not in job_lookup:
+                    continue
+                job_id = job_lookup.get("application_url")
+                for skill in job.get("skills",[]):
+                    if skill not in skill_lookup:
+                        continue
+                    skill_id = skill_lookup.get("skill_name")
+                    junc_list.append({job_id:skill_id})
+            return self.load_get_keys(table,junc_list,["job_id","skill_id"])
+        except Exception as e:
+            logger.exception("Critical error while performing %s: %s", __name__,e)
+
+    def load_job_locations(self, table:str, data:List[Dict[str,Any]],job_keys:List[Dict[str,Any]], location_keys:List[Dict[str,Any]]) -> Union[None,List[Dict[str,Any]]]:
+        try:
+            job_lookup = {job["application_url"]:job["job_id"] for job in job_keys}
+            location_lookup = {(location["city"],location["country"]):location["location_id"] for location in location_keys}
+            jun_list = []
+            for job in data:
+                job_id = job_lookup[job["application_url"]]
+                location_str = job.get('location')
+                parts = location_str.split(",")
+                city = parts[0]
+                country = parts[1] if len(parts)>1 else "Pakistan"
+                loc = (city, country)
+                if loc in location_lookup:
+                    loc_id = location_lookup[loc]
+                    jun_list.append({job_id:loc_id})
+            return self.load_get_keys(table,jun_list,["job_id","location_id"])
+        except Exception as e:
+            logger.exception("Critical error while performing %s: %s", __name__,e)
+
+    def load_job_platforms(self, table:str, data:List[Dict[str,Any]], job_keys:List[Dict[str,Any]], platform_keys:List[Dict[str,Any]]) -> Union[None,List[Dict[str,Any]]]:
+        try:
+            job_lookup = {job["application_url"]:job["job_id"] for job in job_keys}
+            platforms_lookup = {platform["platform_name"]:platform["platform_id"] for platform in platform_keys}
+            junc_list = []
+            for job in data:
+                if job["platform"] not in platforms_lookup:
+                    continue
+                job_id = job_lookup.get("application_url")
+                for platform in job.get("skills", []):
+                    if platform not in platforms_lookup:
+                        continue
+                    platforms_id = platforms_lookup.get("platform_name")
+                    junc_list.append({job_id: platforms_id})
+            return self.load_get_keys(table, junc_list, ["job_id", "platform_id"])
+        except Exception as e:
+            logger.exception("Critical error while performing %s: %s", __name__,e)
